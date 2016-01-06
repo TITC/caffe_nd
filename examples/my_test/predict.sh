d50be57ac4f9c6c8ceb8256d02c6401f1839b02b
Xiaomi2008 <zengtao08@gmail.com>
2016-01-06 15:14:59 -0800

    predict protext example

--- examples/my_test/predict.sh
+++ examples/my_test/predict.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-GLOG_logtostderr=1 /tempspace/tzeng/caffe_nd_sense_segmetation/.build_release/tools/predict_seg.bin deploy_test.prototxt \
-patch_test_snapshot_iter_1000.caffemodel \
+GLOG_logtostderr=1 /tempspace/tzeng/caffe_nd_sense_segmetation/.build_release/tools/predict_seg.bin deploy_bigneuron_ResNET_v2.prototxt \
+patch_test_fcn_ResNET_5pool_snapshot_v2_iter_5367.caffemodel \
 mean_file.binary \
 /tempspace/tzeng/bigneuron/big_neuron_hackthon_tesla/data/hd5_valid.h5 \
-/tempspace/tzeng/bigneuron/big_neuron_hackthon_tesla/data/predict_valid.h5 \
-GPU \
-5
+/tempspace/tzeng/bigneuron/big_neuron_hackthon_tesla/data/ptrfict_bslif.h5 \
+cpu
