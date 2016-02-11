b63641d0de2c1238a82c9c150b74e66436d367a2
Xiaomi2008 <zengtao08@gmail.com>
2016-02-11 13:42:10 -0800

    unpooling added

--- include/caffe/layers/bias_layer.hpp
+++ include/caffe/layers/bias_layer.hpp
@@ -1,34 +1,38 @@
-#ifndef CAFFE_UNPOOLING_LAYER_HPP_
-#define CAFFE_UNPOOLING_LAYER_HPP_
+#ifndef CAFFE_BIAS_LAYER_HPP_
+#define CAFFE_BIAS_LAYER_HPP_
 
 #include <vector>
 
 #include "caffe/blob.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/proto/caffe.pb.h"
+
 namespace caffe {
 
+/**
+ * @brief Computes a sum of two input Blobs, with the shape of the
+ *        latter Blob "broadcast" to match the shape of the former.
+ *        Equivalent to tiling the latter Blob, then computing the elementwise
+ *        sum.
+ *
+ * The second input may be omitted, in which case it's learned as a parameter
+ * of the layer.
+ */
 template <typename Dtype>
-class UnpoolingLayer: public Layer<Dtype> {
+class BiasLayer : public Layer<Dtype> {
  public:
-  explicit UnpoolingLayer(const LayerParameter& param)
+  explicit BiasLayer(const LayerParameter& param)
       : Layer<Dtype>(param) {}
-
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
 
-  virtual inline const char* type() const { return "Unpooling"; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline const char* type() const { return "Bias"; }
   virtual inline int MinBottomBlobs() const { return 1; }
-  // MAX POOL layers can output an extra top blob for the mask;
-  // others can only output the pooled inputs.
-  virtual inline int MaxBottomBlobs() const {
-    return (this->layer_param_.unpooling_param().unpool() ==
-            UnpoolingParameter_UnpoolMethod_MAX) ? 2 : 1;
-  }
- protected:
+  virtual inline int MaxBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -38,14 +42,13 @@ class UnpoolingLayer: public Layer<Dtype> {
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-  int kernel_h_, kernel_w_;
-  int stride_h_, stride_w_;
-  int pad_h_, pad_w_;
-  int channels_;
-  int height_, width_;
-  int unpooled_height_, unpooled_width_;
+ private:
+  Blob<Dtype> bias_multiplier_;
+  int outer_dim_, bias_dim_, inner_dim_, dim_;
 };
+
+
 
 }  // namespace caffe
 
-#endif  // CAFFE_UNPOOLING_LAYERS_HPP_}
+#endif  // CAFFE_BIAS_LAYER_HPP_
