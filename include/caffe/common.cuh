d36bb37bf57ab56755e4f17f8292d7bffc4b14c6
Chris Churas <churas@gmail.com>
2017-08-30 12:21:22 -0700

    added compiler directive to NOT include atomicAdd if using cuda >= 600 cause it is already defined

--- include/caffe/common.cuh
+++ include/caffe/common.cuh
@@ -5,6 +5,11 @@
 
 #include <cuda.h>
 
+// dont add atomicAdd for newer versions of cuda ie 7+
+// https://stackoverflow.com/questions/39274472/error-function-atomicadddouble-double-has-already-been-defined
+ #if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
+
+ #else
 // CUDA: atomicAdd is not defined for doubles
 static __inline__ __device__ double atomicAdd(double *address, double val) {
   unsigned long long int* address_as_ull = (unsigned long long int*)address;
@@ -17,5 +22,5 @@ static __inline__ __device__ double atomicAdd(double *address, double val) {
   } while (assumed != old);
   return __longlong_as_double(old);
 }
-
+ #endif
 #endif
